f744983b65c33f6b134dc9e830ddc679bdb4c912
alinutz310 <popescu.alina@79@yahoo.com>
2024-03-30 15:05:25 +0200

    Create Corpus Version 1

--- scripts/create_corpus.py
+++ scripts/create_corpus.py
@@ -1,3 +1,5 @@
+import os
+
 import typer
 import json
 from collections import Counter
@@ -6,24 +8,40 @@ import spacy
 from spacy.tokens import DocBin, Span
 
 
-citesti tot jsonul tau, nu mai faci conversie
+# citesti tot jsonul tau, nu mai faci conversie
 def make_doc_bin(json_train_sau_test: Path, nlp_dir: Path, out_path: Path):
     """ Step 2: Once we have done the manual annotations with Prodigy, create corpora in spaCy format. """
     nlp = spacy.load(nlp_dir, exclude="parser, tagger")
     docs = DocBin()
     gold_ids = []
-    for elem in jsonultau:
-        sentece = elem["Context"]
+
+    # Load JSON data
+    with open(json_train_sau_test, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+    for elem in json_data:
+        sentence = elem["Context"]
+        # print(sentence)
         doc = nlp.make_doc(sentence)
-        gold_ids.append(qid)
+        # print(doc)
+        # print()
+        start = elem["Start"]
+        stop = elem["Stop"]
+        link_ref = elem["Link_Ref"]
+        gold_ids.append(link_ref)
+        # gold_ids.append(link_ref)
         entity = doc.char_span(
-            Start,
-            Stop,
-            label='LEGAL',
-            kb_id=Link_Ref,
+            start,
+            stop,
+            label = 'LEGAL',
+            kb_id = link_ref
         )
-        doc.ents = [entity]
-        for i, t in enumerate(doc):
+        if entity is not None:
+            doc.ents = [entity]
+        else:
+            print(f"Failed to create entity from {start} to {stop} in context: {sentence[:50]}...")
+            continue
+
+        for i, token in enumerate(doc):
             doc[i].is_sent_start = i == 0
         docs.add(doc)
 
@@ -32,5 +50,17 @@ def make_doc_bin(json_train_sau_test: Path, nlp_dir: Path, out_path: Path):
     print()
     docs.to_disk(out_path)
 
+
 if __name__ == "__main__":
-    typer.run(make_doc_bin)
+    print(os.getcwd())
+    make_doc_bin(
+         json_train_sau_test=Path("all_datas_training_v2.json"),
+         nlp_dir=Path("../temp/my_nlp"),
+         out_path=Path("../corpus/dev_train.spacy")
+     )
+    make_doc_bin(
+        json_train_sau_test=Path("all_datas_test_v2.json"),
+        nlp_dir=Path("../temp/my_nlp"),
+        out_path=Path("../corpus/dev_test.spacy")
+    )
+    # typer.run(make_doc_bin)
